b6bb3d0fad3f8fb16e3cca888f6d1edd76d6b77c
Hemant Kumar <hemantk@codeaurora.org>
2013-02-03 15:56:29 -0800

    usb: gadget: Support multiple HSIC transport instances for dun and rmnet
    Instead of using integer-based IDs to couple bridge instances to the
    corresponding gadget ports, assign transport name strings (passed by
    userspace) describing the port instance, and match strings to identify
    the correct instance when bridge_open() called. This allows multiple
    dun and rmnet ports to be assigned without having dependecy of
    one-to-one mapping between gadget port instance and bridge driver
    instance.
    
    Change-Id: Ia87bd6e9c72d5e98f47dbc2a1cf0b71b48daa1a8
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/usb/gadget/f_rmnet.c
+++ drivers/usb/gadget/f_rmnet.c
@@ -1063,7 +1063,8 @@ static void frmnet_cleanup(void)
 	no_data_hsuart_ports = 0;
 }
 
-static int frmnet_init_port(const char *ctrl_name, const char *data_name)
+static int frmnet_init_port(const char *ctrl_name, const char *data_name,
+		const char *port_name)
 {
 	struct f_rmnet			*dev;
 	struct rmnet_ports		*rmnet_port;
@@ -1101,6 +1102,7 @@ static int frmnet_init_port(const char *ctrl_name, const char *data_name)
 		no_ctrl_smd_ports++;
 		break;
 	case USB_GADGET_XPORT_HSIC:
+		ghsic_ctrl_set_port_name(port_name, ctrl_name);
 		rmnet_port->ctrl_xport_num = no_ctrl_hsic_ports;
 		no_ctrl_hsic_ports++;
 		break;
@@ -1127,6 +1129,7 @@ static int frmnet_init_port(const char *ctrl_name, const char *data_name)
 		no_data_bam2bam_ports++;
 		break;
 	case USB_GADGET_XPORT_HSIC:
+		ghsic_data_set_port_name(port_name, data_name);
 		rmnet_port->data_xport_num = no_data_hsic_ports;
 		no_data_hsic_ports++;
 		break;
